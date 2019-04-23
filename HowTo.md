04ecde7ba56c7bd9cc129f3a9d90792f9655644b
Ali Thompson <alithom123@gmail.com>
2019-04-23 16:25:10 -0500

    api routes added for get all idioms, scrape a search term.

--- HowTo.md
+++ HowTo.md
@@ -13,6 +13,7 @@ App listening on port 3000
 * Add route to a basic html page. 
 * Create index.html page make sure it gets served.
 
+```html
 <!doctype html>
 <html lang="en">
   <head>
@@ -50,6 +51,7 @@ App listening on port 3000
 
   </body>
 </html>
+```
 
 https://i.gyazo.com/bb1f8151b26eb12c655ec9415a9350cc.png
 
@@ -64,4 +66,83 @@ https://i.gyazo.com/4f79e4248540386fcb43a0364eff34d8.png
 
 * You should see a response and a bunch of html.
 * Now let's see if we can use cheerio to pull out the idioms we want. 
+
+```js
+var express = require("express");
+// Require axios to get html of a webpage
+var axios = require("axios");
+// Require cheerio to allow you to scrape html page in a fashion similar to jquery
+var cheerio = require("cheerio");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Make public a static folder
+app.use(express.static("public"));
+
+// Routes
+// ======
+
+// Simple index route
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + "./public/index.html"));
+  });
+
+axios.get("https://idioms.thefreedictionary.com/light").then(function(response) {
+    
+    var $ = cheerio.load(response.data);
+
+    var idioms = [];
+    var listItems = $("ul.idiKw li a").each(function(i, elem) {
+        idioms.push($(elem).text());
+    });
+
+    console.log(idioms);
+});
+
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App listening on port " + PORT);
+});
+```
+
+https://i.gyazo.com/de4f2d264b17dc9e85da67a82ac69e37.png
+
+* As you can see we have found all the idioms.
+* Next we want to connect to MongoDB to store our data.  We'll try to store the results so that if two users make the same query we won't scrape the website again but instead return the stored results.  
+* Install mongoose `npm install mongoose`
+* Create a model folder
+* Create an idioms.js file that describes the schema
+* Connect to mongoose 
+* Add dummy data and test.
+https://i.gyazo.com/555cec7486d82dc334b9bf51aacbd077.png
+
+* Create route to get all idioms in db.
+
+```js 
+// Route for retrieving all idioms
+app.get("/idioms", function(req, res) {
+    // Find all Notes
+    // db.Note.find({})
+    Idiom.find({})
+      .then(function(dbNote) {
+        // If all Notes are successfully found, send them back to the client
+        res.json(dbNote);
+      })
+      .catch(function(err) {
+        // If an error occurs, send the error back to the client
+        res.json(err);
+      });
+  });
+  ```
+
+  
 
--- app.js
+++ app.js
@@ -3,6 +3,33 @@ var express = require("express");
 var axios = require("axios");
 // Require cheerio to allow you to scrape html page in a fashion similar to jquery
 var cheerio = require("cheerio");
+// Require Mongoose to store idioms in database
+var mongoose = require("mongoose");
+
+
+// Requiring the `Idioms` model for accessing the `idioms` collection
+var Idiom = require("./models/idioms.js");
+
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost/idioms_db", { useNewUrlParser: true });
+
+// Create dummy data to save to the database
+var data = {
+    idiom: "a heavy purse makes a light heart",
+    link: "https://idioms.thefreedictionary.com/a+heavy+purse+makes+a+light+heart"
+  };
+  
+  // Save a new Idiom using the data object
+  Idiom.create(data)
+    .then(function(savedIdiom) {
+      // If saved successfully, print the new Idiom document to the console
+      console.log(savedIdiom);
+    })
+    .catch(function(err) {
+      // If an error occurs, log the error message
+      console.log(err.message);
+    });
+  
 
 var PORT = 3000;
 
@@ -17,35 +44,125 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-// Routes
-// ======
+
+
+/* Test Axios with Cheerio */
+axios.get("https://idioms.thefreedictionary.com/light").then(function(response) {
+    
+    var $ = cheerio.load(response.data);
+
+    var idioms = [];
+    var links = [];
+    var listItems = $("ul.idiKw li a").each(function(i, elem) {
+        idioms.push($(elem).text());
+        links.push("https://thefreedictionary.com/" + $(elem).attr("href"));
+    });
+
+    // console.log(idioms);
+    // console.log(links);
+});
+
+var scrape = function(searchTerm) {
+    /* Test Axios with Cheerio */
+    var idioms = [];
+    return axios.get("https://idioms.thefreedictionary.com/" + searchTerm).then(function(response) {
+    
+        var $ = cheerio.load(response.data);
+        
+        // var links = [];
+        var listItems = $("ul.idiKw li a").each(function(i, elem) {
+            let obj = {
+                idiom: $(elem).text(),
+                link: "https://thefreedictionary.com/" + $(elem).attr("href")
+            };
+            // idioms.push($(elem).text());
+            // links.push("https://thefreedictionary.com/" + $(elem).attr("href"));
+            // console.log("in each")
+            // console.log(obj)
+            idioms.push(obj);
+        });
+
+        // console.log(idioms);
+        return idioms;
+        // console.log(links);
+    });
+    // console.log("OUTER");
+    // console.log(idioms);
+}
+
+/* Routes */
 
 // Simple index route
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname + "./public/index.html"));
   });
 
-axios.get("https://idioms.thefreedictionary.com/light").then(function(response) {
-    // console.log(response);
-    var $ = cheerio.load(response.data);
+// Route for retrieving all idioms
+app.get("/idioms", function(req, res) {
+    // Find all Idioms
+    Idiom.find({})
+      .then(function(savedIdioms) {
+        // If all Idioms are successfully found, send them back to the client
+        res.json(savedIdioms);
+      })
+      .catch(function(err) {
+        // If an error occurs, send the error back to the client
+        res.json(err);
+      });
+  });
 
-    // var listItems = $("ul.idiKw").find("a").attr("href");
 
-    var idioms = [];
-    var listItems = $("ul.idiKw li a").each(function(i, elem) {
-        console.log($(elem).text());
-        idioms.push($(elem).text());
+// Route for retrieving idioms that have an entered string
+app.get("/idioms/search/:searchTerm", function(req, res) {
+  Idiom.find({ "idiom": { "$regex": req.params.searchTerm, "$options": "i" } })
+    .then(function(foundIdioms) {
+        res.json(foundIdioms);
+    })
+    .catch(function(err) {
+        res.json(err);
+    });
+});
+
+// Route to go out and scrape for idioms that have an entered string
+app.get("/idioms/scrape/:searchTerm", function(req, res) {
+
+    console.log("api");
+    scrape(req.params.searchTerm)
+    .then(function(foundIdioms) {
+        console.log("scraped:");
+        console.log(foundIdioms);
+        // Save scraped Idioms
+        foundIdioms.forEach(function(eachIdiom) {
+            Idiom.create(eachIdiom)
+            .then(function(savedIdiom) {
+                // If saved successfully, print the new Idiom document to the console
+                console.log(savedIdiom);
+            })
+            .catch(function(err) {
+                // If an error occurs, log the error message
+                console.log(err.message);
+            });
+        })
+        
+
+        res.json(foundIdioms);
+    })
+    .catch(function(err) {
+        res.json(err);
     });
 
-    // .each(function(i, elem) {
-    //     fruits[i] = $(this).text();
-    //   });
+  });
 
-    console.log(idioms);
+  // Route to go out and scrape for idioms that have an entered string
+app.get("/idioms/drop", function(req, res) {
 
-    // console.log(listItems);
-    // $(".idiKw").each(function(i, element) {
-    // }
+    Idiom.deleteMany({})
+    .then(function(res) {
+        res.json(res)
+    })
+    .catch(function(err) {
+        res.json(err)
+    })
 });
 
 
