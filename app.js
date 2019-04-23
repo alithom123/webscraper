37c6e907c3f23ad186580518ce669775c0395230
Ali Thompson <alithom123@gmail.com>
2019-04-23 12:16:17 -0500

    installed express. made route for index.html page and made sure that basic html page was being served up.

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Make public a static folder
+app.use(express.static("public"));
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App listening on port " + PORT);
+});
